4f11dcf06d8275a120e1ffb95a5fa9c8d3c7c1c4
Anon Sols <soloschmail@gmail.com>
2024-04-13 09:03:51 +0100

    currency sign fix

--- src/features/cart/CartOverview.tsx
+++ src/features/cart/CartOverview.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getQuantity, getTotalPrice } from "./CartSlice";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
   const cartQuantity = useSelector(getQuantity);
@@ -11,7 +12,7 @@ function CartOverview() {
     <div className="bg-stone-800 text-stone-200 p-4 uppercase sm:px-6 text-sm md:text-base flex items-center justify-between  ">
       <p className="text-stone-300 space-x-4 font-semibold uppercase sm:space-x-6">
         <span>{cartQuantity} pizzas</span>
-        <span>${cartPrice}</span>
+        <span>{formatCurrency(cartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
